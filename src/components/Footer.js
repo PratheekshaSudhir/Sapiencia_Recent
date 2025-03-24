e3cac6689ee277830d095a295600e55556d92016
RashmithaRB <rbangerarashmitha@gmail.com>
2025-03-24 09:31:40 +0530

    Footer info updated including social icons in contact div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import { FaFacebookF, FaTwitter, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -11,7 +12,11 @@ const Footer = () => {
             AI-Driven Security &amp; Intelligence for Healthcare &amp; Defense,<br />
             ensuring the highest level of compliance and protection.
           </p>
+
+          
         </div>
+                
+
         <div className="footer-links">
           <h4>Solutions</h4>
           <ul>
@@ -22,15 +27,23 @@ const Footer = () => {
         </div>
         <div className="footer-contact">
           <h4>Contact</h4>
-         
-
-
-
           <p>dabis.camero@sapiencia.ai</p>
           <p>+1 (223) 225-0608</p>
-          <p> 1200 Market Street, Unit 17, PMB # 184, Lemoyne, Pennsylvania 17043, United States
-          </p>
+          <p> 1200 Market Street, Unit 17, PMB # 184, Lemoyne, Pennsylvania 17043, United States</p>
+
+          {/* Social Icons Container */}
+        <div className="icon-container">
+            <a href="#"><FaFacebookF className="w-5 h-5" /></a>
+            <a href="#"><FaTwitter className="w-5 h-5" /></a>
+            <a href="#"><FaGithub className="w-5 h-5" /></a>
+            <a href="#"><FaInstagram className="w-5 h-5" /></a>
+          </div>
+
         </div>
+
+        
+
+
       </div>
       <div className="footer-bottom">
         <p>© {new Date().getFullYear()} Sapiencia.AI. All rights reserved.</p>
