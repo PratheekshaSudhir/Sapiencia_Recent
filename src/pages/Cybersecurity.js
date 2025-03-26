9a25a0399b66abc9df22ad75829b5f140667efdc
RashmithaRB <rbangerarashmitha@gmail.com>
2025-03-26 11:05:49 +0530

    footer in cybersecurity page modified

--- src/pages/Cybersecurity.js
+++ src/pages/Cybersecurity.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import AnimatedBackground from "../components/AnimatedBackground";
 import "./Cybersecurity.css";
+import Footer from "../components/Footer";
 import { FaCheck } from "react-icons/fa";
 
 const Cybersecurity = () => {
